6c1e63c1e4e616af04c75446da205bef85e5d0b1
Jeeva-K7 <116611557+Jeeva-K7@users.noreply.github.com>
2024-08-13 20:26:05 +0530

    Create Pattern-1.c

---
+++ Pattern-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num=7;
+    
+    for(int i=1; i<=num; i++){
+        for(int j=1; j<=i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}

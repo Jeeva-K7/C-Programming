7383a6ce0921b2f043ac23862cbb1c0c5db12331
Jeeva-K7 <116611557+Jeeva-K7@users.noreply.github.com>
2024-08-26 19:49:14 +0530

    Create MatrixMultiplication.c
    Enter Matrix-1 row and col: 3 2
    Enter Matrix-1 Values:
    1 4
    2 5
    3 6
    Enter Matrix-2 row and col: 2 3
    Enter Matrix-1 Values:
    7 -8 9
    10 11 12
    
    [Resultant Matrix]
    47	36	57	
    64	39	78	
    81	42	99

---
+++ MatrixMultiplication.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main(){
+    int r1, c1, r2, c2;
+    printf("Enter Matrix-1 row and col: ");
+    scanf("%d %d",&r1,&c1);
+    int mat1[r1][c1];
+
+    printf("Enter Matrix-1 Values:\n");
+    for(int i=0; i<r1; i++){
+        for(int j=0; j<c1; j++){
+            scanf("%d",&mat1[i][j]);
+        }
+    }
+
+    printf("Enter Matrix-2 row and col: ");
+    scanf("%d %d",&r2,&c2);
+    int mat2[r2][c2];
+    
+    printf("Enter Matrix-1 Values:\n");
+    for(int i=0; i<r2; i++){
+        for(int j=0; j<c2; j++){
+            scanf("%d",&mat2[i][j]);
+        }
+    }
+    
+    if(c1 != r2) {
+        printf("Matrix multiplication is not possible.\n");
+        return 1;
+    }
+
+
+    
+    int product[r1][c2];
+    int sum=0;
+    for(int i=0; i<r1; i++){
+        for(int j=0; j<c2; j++){
+            for(int k=0; k<c1;k++){
+                sum+= mat1[i][k]*mat2[k][j]; 
+            }
+            product[i][j]=sum;
+            sum=0;
+            
+            printf("%d\t",product[i][j]);
+        }
+        
+        printf("\n");
+    }
+    
+
+}
